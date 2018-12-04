86d653f9c36587780957335a1cdfe0086672c428
Olga Kashirina <olga@onetbook.localdomain>
2018-12-04 21:14:06 +0200

    оно теперь должно работать через апи, если вы доделаете контроллеры

---
+++ src/main/java/com/example/remy/models/Constants.java
@@ -0,0 +1,8 @@
+package com.example.remy.models;
+
+public class Constants {
+    public final static String URL_OBJECTS = "http://localhost:8080/botapi/objects";
+    public final static String URL_PARAMS = "http://localhost:8080/botapi/params";
+    public final static String URL_OBJECTTYPES = "http://localhost:8080/botapi/objecttypes";
+    public final static String URL_ATTRIBUTES = "http://localhost:8080/botapi/attributes";
+}
